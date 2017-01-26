5fd8420dad69aa3ffd10630570dcf87fde682007
Patrick Marino <Patrick Marino>
2017-01-26 11:56:05 -0500

    Finished project, improved get and push functions then created function to avoid repetitive code

--- main.py
+++ main.py
@@ -17,21 +17,34 @@
 import webapp2
 import caesar
 
+def build_page(textarea_content):
+    rot_label = "<label>Rotate by:</label>"
+    rotation_input = "<input type='number' name='rotation' />"
+
+    message_label = "<label>Type a message:</label>"
+    textarea = "<textarea name='message'>" + textarea_content + "</textarea>"
+    submit = "<input type='submit' />"
+    form = ("<form method='post'>" +
+        rot_label + rotation_input + "<br>" +
+        message_label + textarea +
+        "<br>" + submit + "</form>")
+    header = "<h2>Web Caesar</h2>"
+
+    return header + form
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        content = build_page("")
 
-        rotation_input = "<input type='number' name='rotation' />"
-        textarea = "<textarea name='message'></textarea>"
-        submit = "<input type='submit' />"
-        form = "<form method='post'>" + rotation_input + textarea + "<br>" + submit + "</form>"
-
-        self.response.write(form)
+        self.response.write(content)
 
     def post(self):
         message = self.request.get("message")
         rotation = int(self.request.get('rotation'))
         encrypted_message = caesar.encrypt(message, rotation)
-        self.response.write("Secret Mesage: " + encrypted_message)
+        content = build_page(encrypted_message)
+
+        self.response.write(content)
 
 
 app = webapp2.WSGIApplication([
