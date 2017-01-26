d4da7a1e917a7c771169bbacc61bb8fa1fc461bf
Patrick Marino <Patrick Marino>
2017-01-26 01:02:03 -0500

    going to bed!  everything looks good, but need to format

--- main.py
+++ main.py
@@ -19,14 +19,19 @@ import caesar
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        message = "Hello World!"
-        encrypted_message = caesar.encrypt(message, 13)
 
-        textarea = "<textarea>" + encrypted_message + "</textarea>"
+        rotation_input = "<input type='number' name='rotation' />"
+        textarea = "<textarea name='message'></textarea>"
         submit = "<input type='submit' />"
-        form = "<form>" + textarea + "<br>" + submit + "</form>"
+        form = "<form method='post'>" + rotation_input + textarea + "<br>" + submit + "</form>"
 
         self.response.write(form)
+
+    def post(self):
+        message = self.request.get("message")
+        rotation = int(self.request.get('rotation'))
+        encrypted_message = caesar.encrypt(message, rotation)
+        self.response.write("Secret Mesage: " + encrypted_message)
 
 
 app = webapp2.WSGIApplication([
